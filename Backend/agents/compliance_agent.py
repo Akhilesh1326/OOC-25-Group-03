d1a50de068e0b76aa958f0a55f1234e38bc07436
Akhilesh26 <akhileshpimple3@gmail.com>
2025-04-05 19:20:26 +0530

    change complicance data dir fetching

--- Backend/agents/compliance_agent.py
+++ Backend/agents/compliance_agent.py
@@ -1,6 +1,20 @@
+import os
 import json
 
-with open("data/company_profile.json") as f:
+# Get the absolute path to the current file (compliance_agent.py)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Move up to the Backend directory
+backend_dir = os.path.dirname(current_dir)
+
+# Move up to the project root (OOC25)
+project_root = os.path.dirname(backend_dir)
+
+# Construct the path to data/compliance_data.json
+json_path = os.path.join(project_root, "data", "compliance_data.json")
+
+# Load the JSON
+with open(json_path, "r") as f:
     profile = json.load(f)
 
 def check_compliance(analysis):
