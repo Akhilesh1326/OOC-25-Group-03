dd8a4293e1d781d5690e42a72bd45468a2016ef2
Akhilesh26 <akhileshpimple3@gmail.com>
2025-04-05 23:27:02 +0530

    add router code

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route, createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Dashboard from './dashboard/Dashboard.jsx'
+import About from './about/About.jsx'
+
+const router = createBrowserRouter([
+    {path: "/", element: <App/>},
+    {path: "/dashboard", element: <Dashboard/>},
+    {path: "/about", element: <About/>}
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+ <RouterProvider router={router} />
   </StrictMode>,
 )
