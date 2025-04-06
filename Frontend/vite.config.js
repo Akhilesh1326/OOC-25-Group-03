2bfc3a403cc986aa8f224e26e3aa567e2606cb4a
Akhilesh26 <akhileshpimple3@gmail.com>
2025-04-06 09:16:47 +0530

    add proxy server for all api call to the backend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 // DO NOT import any CSS or JSX here
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8000', // Adjust the port to match your Node.js backend
+    },
+  },
 });
